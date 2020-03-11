50e3ee03e7e318576a21dd978babb0dad14ebdbe
имя_пользователя_github <__>
2020-03-11 14:08:33 +0600

    First commit

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+
+	$('#my-menu').mmenu(
+	{
+		extensions: [ 'theme-black', 'fx-menu-slide', 'pagedim-black', "position-right"],
+		navbar: {
+			title: '<img src="img/logo_burger.svg" alt="Логотип">'
+		},
+		offCanvas: {
+			"position": "right"
+		}
+	});
+	
+	$('.popup').magnificPopup();
+
+});
